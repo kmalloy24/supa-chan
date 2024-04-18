0a5fd1ee252aba64ceafb21ed15aa306d4177d7f
Kyle Malloy <kmalloy24@gmail.com>
2024-04-18 01:25:44 -0400

    data fetch posts

--- src/routes/s/+page.svelte
+++ src/routes/s/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import GradientText from '$lib/components/GradientText.svelte';
+	import { onMount } from 'svelte';
 
 	export let data;
 	$: ({ session, supabase } = data);
@@ -7,6 +8,20 @@
 	function trimUserId(userId: string): string {
 		return userId.slice(-5);
 	}
+
+	let posts = [];
+
+	async function fetchPosts() {
+		const { data, error } = await supabase.from('posts').select('*');
+
+		if (error) {
+			console.error('Error fetching posts:', error);
+		} else {
+			posts = data;
+		}
+	}
+
+	onMount(fetchPosts);
 </script>
 
 <div class="container mx-auto flex justify-center items-center my-8">
@@ -37,15 +52,16 @@
 <hr class="!border-t-2 !border-primary-500 mx-24" />
 
 <div class="mx-auto my-8 gap-y-8 flex flex-col">
-	<div class="card mx-4">
-		<section class="p-4">
-			Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque interdum rutrum sodales.
-			Nullam mattis fermentum libero, non volutpat.
-		</section>
-		<footer class="card-footer flex justify-end">
-			<p class="text-2xl font-bold">
-				<GradientText>c2296</GradientText>
-			</p>
-		</footer>
-	</div>
+	{#each posts as post (post.id)}
+		<div class="card mx-4">
+			<section class="p-4">
+				{post.content}
+			</section>
+			<footer class="card-footer flex justify-end">
+				<p class="text-2xl font-bold">
+					<GradientText>{trimUserId(post.user_id)}</GradientText>
+				</p>
+			</footer>
+		</div>
+	{/each}
 </div>
